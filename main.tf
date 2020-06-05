8efd993d99c7d13cb4892d81dfc129d7dba4ccd4
liangchen0323 <chenliang323@hotmail.com>
2020-06-05 10:18:05 +0100

    Fix typo

--- main.tf
+++ main.tf
@@ -151,7 +151,7 @@ resource "aws_route53_record" "www" {
 
   alias {
     name    = aws_cloudfront_distribution.s3_distribution.domain_name
-    zone_id = aws_cloudfront_distribution.s3_distribution.hostsed_zone_id
+    zone_id = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
