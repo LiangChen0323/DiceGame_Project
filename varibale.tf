79a363f21126a140861d735d52f8e722fb23c87a
Liang Chen <chenliang323@hotmail.com>
2020-06-05 12:29:49 +0100

    Update varibale.tf

--- varibale.tf
+++ varibale.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {}
-variable my-access-key {}
-variable my-secret-key {}
+variable "my-access-key" {}
+variable "my-secret-key" {}
 variable "bucket_name" {}
 variable "domain_name" {}
 variable "zone_id" {}
